14a9984c9005ea7e4d2dc98659aaddc57369b7ba
unknown <kadekbayu322@gmail.com>
2026-07-08 11:46:46 +0800

    edit AppService

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (request()->header('X-Forwarded-Proto') === 'https' || env('APP ENV') === 'production') {
+        if (request()->header('X-Forwarded-Proto') === 'https' || env('APP_ENV') === 'production') {
             URL:: forceScheme('https');
         }
     }
